e0c614b2f78b9450b5197a805d55d92ab34d33ed
Massimo Guarnieri <massimo.guarnieri@polimi.it>
2019-06-30 15:41:43 +0200

    Simplified deserialization code.

--- src/lcfr/arch/endianness.h
+++ src/lcfr/arch/endianness.h
@@ -3,202 +3,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#if defined(__GLIBC__)
-# include <endian.h>
-#endif
-#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__APPLE__)
-# include <sys/types.h>
-#elif defined (__NetBSD__) || defined(__OpenBSD__)
-# include <sys/param.h>
-#endif
+namespace lcfr {
 
-/* if x86 compatible cpu */
-#if defined(i386) || defined(__i386__) || defined(__i486__) || \
-	defined(__i586__) || defined(__i686__) || defined(__pentium__) || \
-	defined(__pentiumpro__) || defined(__pentium4__) || \
-	defined(__nocona__) || defined(prescott) || defined(__core2__) || \
-	defined(__k6__) || defined(__k8__) || defined(__athlon__) || \
-	defined(__amd64) || defined(__amd64__) || \
-	defined(__x86_64) || defined(__x86_64__) || defined(_M_IX86) || \
-	defined(_M_AMD64) || defined(_M_IA64) || defined(_M_X64)
-/* detect if x86-64 instruction set is supported */
-# if defined(_LP64) || defined(__LP64__) || defined(__x86_64) || \
-	defined(__x86_64__) || defined(_M_AMD64) || defined(_M_X64)
-#  define CPU_X64
-# else
-#  define CPU_IA32
-# endif
-#endif
+uint16_t reverse(uint16_t x);
+uint32_t reverse(uint32_t x);
+uint64_t reverse(uint64_t x);
 
-#define ML_BYTE_ORDER_LE 1234
-#define ML_BYTE_ORDER_BE 4321
-
-#if (defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN) && __BYTE_ORDER == __LITTLE_ENDIAN) || \
-    (defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
-#  define ML_BYTE_ORDER ML_BYTE_ORDER_LE
-#elif (defined(__BYTE_ORDER) && defined(__BIG_ENDIAN) && __BYTE_ORDER == __BIG_ENDIAN) || \
-      (defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-#  define ML_BYTE_ORDER ML_BYTE_ORDER_BE
-#elif defined(_BYTE_ORDER)
-#  if defined(_LITTLE_ENDIAN) && (_BYTE_ORDER == _LITTLE_ENDIAN)
-#    define ML_BYTE_ORDER ML_BYTE_ORDER_LE
-#  elif defined(_BIG_ENDIAN) && (_BYTE_ORDER == _BIG_ENDIAN)
-#    define ML_BYTE_ORDER ML_BYTE_ORDER_BE
-#  endif
-#elif defined(__sun) && defined(_LITTLE_ENDIAN)
-#  define ML_BYTE_ORDER ML_BYTE_ORDER_LE
-#elif defined(__sun) && defined(_BIG_ENDIAN)
-#  define ML_BYTE_ORDER ML_BYTE_ORDER_BE
-#endif
-
-/* try detecting endianness by CPU */
-#ifdef ML_BYTE_ORDER
-#elif defined(CPU_IA32) || defined(CPU_X64) || defined(__ia64) || defined(__ia64__) || \
-      defined(__alpha__) || defined(_M_ALPHA) || defined(vax) || defined(MIPSEL) || \
-      defined(_ARM_) || defined(__arm__)
-#  define ML_BYTE_ORDER ML_BYTE_ORDER_LE
-#elif defined(__sparc) || defined(__sparc__) || defined(sparc) || \
-      defined(_ARCH_PPC) || defined(_ARCH_PPC64) || defined(_POWER) || \
-      defined(__POWERPC__) || defined(POWERPC) || defined(__powerpc) || \
-      defined(__powerpc__) || defined(__powerpc64__) || defined(__ppc__) || \
-      defined(__hpux)  || defined(_MIPSEB) || defined(mc68000) || \
-      defined(__s390__) || defined(__s390x__) || defined(sel)
-# define ML_BYTE_ORDER ML_BYTE_ORDER_BE
-#else
-#  error "Can't detect CPU architechture"
-#endif
-
-#define IS_BIG_ENDIAN (ML_BYTE_ORDER == ML_BYTE_ORDER_BE)
-#define IS_LITTLE_ENDIAN (ML_BYTE_ORDER == ML_BYTE_ORDER_LE)
-
-#ifndef __has_builtin
-# define __has_builtin(x) 0
-#endif
-
-#define IS_ALIGNED_32(p) (0 == (3 & ((const char*)(p) - (const char*)0)))
-#define IS_ALIGNED_64(p) (0 == (7 & ((const char*)(p) - (const char*)0)))
-
-#if defined(_MSC_VER)
-#define ALIGN_ATTR(n) __declspec(align(n))
-#elif defined(__GNUC__)
-#define ALIGN_ATTR(n) __attribute__((aligned (n)))
-#else
-#define ALIGN_ATTR(n) /* nothing */
-#endif
-
-
-#if defined(_MSC_VER) || defined(__BORLANDC__)
-#define I64(x) x##ui64
-#else
-#define I64(x) x##ULL
-#endif
-
-
-#ifndef __STRICT_ANSI__
-#define ML_INLINE inline
-#elif defined(__GNUC__)
-#define ML_INLINE __inline__
-#else
-#define ML_INLINE
-#endif
-
-/* define ml_ctz - count traling zero bits */
-#if (defined(__GNUC__) && __GNUC__ >= 4 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)) || \
-    (defined(__clang__) && __has_builtin(__builtin_ctz))
-/* GCC >= 3.4 or clang */
-# define ml_ctz(x) __builtin_ctz(x)
-#else
-unsigned ml_ctz(unsigned); /* define as function */
-#endif
-
-void ml_swap_copy_str_to_u16(void* to, int index, const void* from, size_t length);
-void ml_swap_copy_str_to_u32(void* to, int index, const void* from, size_t length);
-void ml_swap_copy_str_to_u64(void* to, int index, const void* from, size_t length);
-void ml_swap_copy_u64_to_str(void* to, const void* from, size_t length);
-void ml_u32_mem_swap(unsigned *p, int length_in_u32);
-
-/* bswap definitions */
-#if (defined(__GNUC__) && (__GNUC__ >= 4) && (__GNUC__ > 4 || __GNUC_MINOR__ >= 3)) || \
-    (defined(__clang__) && __has_builtin(__builtin_bswap32) && __has_builtin(__builtin_bswap64))
-/* GCC >= 4.3 or clang */
-# define bswap_16(x) __builtin_bswap16(x)
-# define bswap_32(x) __builtin_bswap32(x)
-# define bswap_64(x) __builtin_bswap64(x)
-#elif (_MSC_VER > 1300) && (defined(CPU_IA32) || defined(CPU_X64)) /* MS VC */
-# define bswap_16(x) _byteswap_ushort((unsigned long)x)
-# define bswap_32(x) _byteswap_ulong((unsigned long)x)
-# define bswap_64(x) _byteswap_uint64((__int64)x)
-#else
-/* fallback to generic bswap definition */
-static ML_INLINE uint16_t bswap_16(uint16_t x)
+#if BIG_ENDIAN
+static void be_serialize(uint8_t* to, uint64_t from)
 {
-    return uint16_t(x >> 8) | uint16_t(x << 8);
+    *(uint64_t*)to = from;
 }
-static ML_INLINE uint32_t bswap_32(uint32_t x)
-{
-# if defined(__GNUC__) && defined(CPU_IA32) && !defined(__i386__) && !defined(ML_NO_ASM)
-	__asm("bswap\t%0" : "=r" (x) : "0" (x)); /* gcc x86 version */
-	return x;
-# else
-	x = ((x << 8) & 0xFF00FF00u) | ((x >> 8) & 0x00FF00FFu);
-	return (x >> 16) | (x << 16);
-# endif
-}
-static ML_INLINE uint64_t bswap_64(uint64_t x)
-{
-	union {
-		uint64_t ll;
-		uint32_t l[2];
-	} w, r;
-	w.ll = x;
-	r.l[0] = bswap_32(w.l[1]);
-	r.l[1] = bswap_32(w.l[0]);
-	return r.ll;
-}
-#endif /* bswap definitions */
 
-#if IS_BIG_ENDIAN
-# define be2me_16(x) (x)
-# define be2me_32(x) (x)
-# define be2me_64(x) (x)
-# define le2me_16(x) bswap_16(x)
-# define le2me_32(x) bswap_32(x)
-# define le2me_64(x) bswap_64(x)
-
-# define be16_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define le16_copy(to, index, from, length) ml_swap_copy_str_to_u16((to), (index), (from), (length))
-# define be32_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define le32_copy(to, index, from, length) ml_swap_copy_str_to_u32((to), (index), (from), (length))
-# define be64_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define le64_copy(to, index, from, length) ml_swap_copy_str_to_u64((to), (index), (from), (length))
-# define me64_to_be_str(to, from, length) memcpy((to), (from), (length))
-# define me64_to_le_str(to, from, length) ml_swap_copy_u64_to_str((to), (from), (length))
-
-#else /* IS_BIG_ENDIAN */
-# define be2me_16(x) bswap_16(x)
-# define be2me_32(x) bswap_32(x)
-# define be2me_64(x) bswap_64(x)
-# define le2me_16(x) (x)
-# define le2me_32(x) (x)
-# define le2me_64(x) (x)
-
-# define be16_copy(to, index, from, length) ml_swap_copy_str_to_u16((to), (index), (from), (length))
-# define le16_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define be32_copy(to, index, from, length) ml_swap_copy_str_to_u32((to), (index), (from), (length))
-# define le32_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define be64_copy(to, index, from, length) ml_swap_copy_str_to_u64((to), (index), (from), (length))
-# define le64_copy(to, index, from, length) memcpy((to) + (index), (from), (length))
-# define me64_to_be_str(to, from, length) ml_swap_copy_u64_to_str((to), (from), (length))
-# define me64_to_le_str(to, from, length) memcpy((to), (from), (length))
-#endif /* IS_BIG_ENDIAN */
-
-/* ROTL/ROTR macros rotate a 32/64-bit word left/right by n bits */
-#define ROTL32(dword, n) ((dword) << (n) ^ ((dword) >> (32 - (n))))
-#define ROTR32(dword, n) ((dword) >> (n) ^ ((dword) << (32 - (n))))
-#define ROTL64(qword, n) ((qword) << (n) ^ ((qword) >> (64 - (n))))
-#define ROTR64(qword, n) ((qword) >> (n) ^ ((qword) << (64 - (n))))
-
-#if IS_BIG_ENDIAN
 static void be_serialize(uint8_t* to, uint32_t from)
 {
     *(uint32_t*)to = from;
@@ -207,6 +23,11 @@ static void be_serialize(uint8_t* to, uint32_t from)
 static void be_serialize(uint8_t* to, uint16_t from)
 {
     *(uint16_t*)to = from;
+}
+
+static void be_deserialize(uint64_t& to, const uint8_t* from)
+{
+    to = *(uint64_t*)from;
 }
 
 static void be_deserialize(uint32_t& to, const uint8_t* from)
@@ -219,23 +40,35 @@ static void be_deserialize(uint16_t& to, const uint8_t* from)
     to = *(uint16_t*)from;
 }
 #else
+static void be_serialize(uint8_t* to, uint64_t from)
+{
+    *(uint64_t*)to = reverse(from);
+}
+
 static void be_serialize(uint8_t* to, uint32_t from)
 {
-    *(uint32_t*)to = bswap_32(from);
+    *(uint32_t*)to = reverse(from);
 }
 
 static void be_serialize(uint8_t* to, uint16_t from)
 {
-    *(uint16_t*)to = bswap_16(from);
+    *(uint16_t*)to = reverse(from);
+}
+
+static void be_deserialize(uint64_t& to, const uint8_t* from)
+{
+    to = reverse(*(uint64_t*)from);
 }
 
 static void be_deserialize(uint32_t& to, const uint8_t* from)
 {
-    to = bswap_32(*(uint32_t*)from);
+    to = reverse(*(uint32_t*)from);
 }
 
 static void be_deserialize(uint16_t& to, const uint8_t* from)
 {
-    to = bswap_16(*(uint16_t*)from);
+    to = reverse(*(uint16_t*)from);
 }
 #endif
+
+}
